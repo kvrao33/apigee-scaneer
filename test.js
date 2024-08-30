fad8a64d7d29b4fbb8142534efc0a4c6005f1959
kvrao33 <karthik.rao@niveussolutions.com>
2024-08-30 09:32:10 +0530

    added get Proxy bundle function

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 import generateToken from "./utility/generateToken.js";
+import downloadAndUnzipProxy from "./utility/getProxyBundle.js";
 
-await generateToken()
+
+const orgName = 'third-octagon-427015-c4';
+const proxyName = 'hello-world';
+const rev = 1;
+const token = await generateToken();
+
+downloadAndUnzipProxy(orgName, proxyName, rev, token);
---
+++ utility/getProxyBundle.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import fs from 'fs';
+import unzipper from 'unzipper';
+import path from 'path';
+
+const downloadAndUnzipProxy = async (orgName, proxyName,rev, token) => {
+  const apiUrl = `https://apigee.googleapis.com/v1/organizations/${orgName}/apis/${proxyName}/revisions/${rev}?format=bundle`;
+  const localFilePath = path.join(process.cwd()+'/proxyBundle', `${proxyName}.zip`);
+  const localUnzipPath = path.join(process.cwd()+'/proxyBundle', proxyName+"_rev"+rev);
+
+  try {
+    console.log(`Downloading proxy bundle from: ${apiUrl}`);
+    
+    // Download the proxy bundle
+    const response = await axios({
+      method: 'get',
+      url: apiUrl,
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+      responseType: 'stream'
+    });
+
+    // Save the downloaded bundle to a local file
+    const writer = fs.createWriteStream(localFilePath);
+    response.data.pipe(writer);
+
+    // Wait until the file is fully written
+    await new Promise((resolve, reject) => {
+      writer.on('finish', resolve);
+      writer.on('error', reject);
+    });
+
+    // Unzip the downloaded file
+    await fs.createReadStream(localFilePath)
+      .pipe(unzipper.Extract({ path: localUnzipPath }))
+      .promise();
+
+    console.log(`Proxy bundle downloaded and unzipped to ${localUnzipPath}`);
+  } catch (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error('Error response data:', error.response.data);
+      console.error('Error response status:', error.response.status);
+      console.error('Error response headers:', error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error('Error request:', error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error('Error message:', error.message);
+    }
+    console.error('Error config:', error.config);
+  } finally {
+    // Clean up the zip file if needed
+    fs.unlinkSync(localFilePath);
+  }
+};
+
+export default downloadAndUnzipProxy;
+
+// Example usage
+// downloadAndUnzipProxy('third-octagon-427015-c4', 'hello-world', 'eval', 'YOUR_ACCESS_TOKEN_HERE');
